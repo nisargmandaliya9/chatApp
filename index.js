660f090cc6708fcee0d36da633f022ed867d4a99
Nisarg Mandaliya <nisarg.soni951@yahoo.com>
2022-08-16 12:54:53 +0530

    deploy(chat app)

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const socketIO = require('socket.io')(httpServer);
+let connectedUsers = {};
+
+app.use(express.static(__dirname + "/view"));
+
+socketIO.on('connection', function(client) {
+
+    client.on('joinRoom', function(req, callback){
+        
+        if(req.room.replace(/\s/g, "").length > 0 && req.username.replace(/\s/g, "").length > 0){
+            
+            let nameTaken = false;
+            
+            Object.keys(connectedUsers).forEach(function(socketId){
+                
+                let userInfo = connectedUsers[socketId];
+                
+                if(userInfo.username.toUpperCase() === req.username.toUpperCase()){
+                    nameTaken = true;
+                }
+            });
+            if(nameTaken){
+                callback({
+                    nameAvail: false,
+                    error: "This name is already taken!"
+                });
+            } else{ 
+                connectedUsers[client.id] = req; 
+                client.join(req.room);
+                client.broadcast.to(req.room).emit('message', {
+                    username: 'System',
+                    text: req.username + ' has joined!'
+                });
+                callback({
+                    nameAvail: true
+                });
+            }
+        } else{
+            callback({
+                nameAvail: false,
+                error: "Form's not completed yet..."
+            });
+        }
+    })
+    
+    client.on('message', function(msg){
+        client.emit("message", msg);
+    });
+    
+    client.emit('message', {
+        username: 'System',
+        text: "Hey there! Ask someone to join the chat room."
+    })
+    
+    client.on('disconnect', function(){
+        
+        let userInfo = connectedUsers[client.id];
+
+        if (typeof userInfo !== 'undefined') {
+            client.leave(connectedUsers[client.id]);
+            socketIO.emit('message',{
+                username: 'System',
+                text: userInfo.username + ' has left!'
+            });
+            delete connectedUsers[client.id];
+        }
+       
+    });
+
+});
+
+httpServer.listen(process.env.PORT || 3000, function(){
+    console.log("Listening on port 3000");
+})
---
+++ view/script.js
@@ -0,0 +1,65 @@
+    let socket = io();
+
+    let body = document.getElementById('body');
+    let loginArea = document.getElementById('login-area');
+    let loginForm = document.getElementById('login-form');
+    let loginInput = document.getElementById('login-input');
+    let errMessage = document.getElementById('error-msg');
+    let optionVal = document.getElementById('room-names');
+    let roomTitle = document.getElementById('room-title');
+    let messageArea = document.getElementById('message-area');
+    let messages = document.getElementById('messages');
+    let chatForm = document.getElementById('chat-form');
+    let chatInput = document.getElementById('chat-input');
+
+    
+    socket.on('connect', function(){
+        loginForm.addEventListener('submit', function(e){
+            e.preventDefault();
+            
+            socket.emit('joinRoom', {
+                username: loginInput.value,
+                room: optionVal.value
+            }, function(data){
+                if (data.nameAvail) {
+                    console.log(body.offsetWidth);
+                    if (optionVal.value == "Gryffindor") {
+                        body.style.backgroundImage = `url("${optionVal.value}.jpg")`;
+                    } else if(optionVal.value == "Slytherin"){
+                        body.style.backgroundImage = `url("${optionVal.value}.jpg")`;
+                    } else if(optionVal.value == "Hufflepuff"){
+                        body.style.backgroundImage = `url("${optionVal.value}.jpg")`;
+                    } else if(optionVal.value == "Ravenclaw"){
+                        body.style.backgroundImage = `url("${optionVal.value}.jpg")`;
+                    }
+                    roomTitle.innerText = `You are in the room: ${optionVal.value}`;
+                    messageArea.style.display = "block";
+                    loginArea.style.display = "none";
+                } else{
+                    errMessage.innerText = `${data.error}`;
+                }
+            });
+        });
+    })
+
+    chatForm.addEventListener('submit', function(e) {
+        e.preventDefault(); 
+        
+        if (chatInput.value) {    
+            socket.emit('message', {
+                username: loginInput.value,
+                text: chatInput.value
+            },
+            chatInput.value);
+            chatInput.value = '';
+        }
+    });
+
+    socket.on('message', function(msg) {
+        
+        let item = document.createElement('li');
+        item.innerHTML = `<b>${msg.username}: </b>${msg.text}`;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+
+    });
